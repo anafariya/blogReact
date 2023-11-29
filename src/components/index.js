95c566bd2c57dbeadd04ec0a0dbdad4adda29f24
Ana Fariya <anafariya@gmail.com>
2023-11-29 14:12:04 +0530

    react blog

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
